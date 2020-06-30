79583f0435c5b63b4a8c435c97d61b4cea44f7ee
Yusuke Kobayashi <ykoba69@gmail.com>
2020-06-30 17:10:26 +0900

    Modifications for current my system

---
+++ elpa/google-c-style-20180130.1736/google-c-style-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "google-c-style" "20180130.1736" "Google's C/C++ style for c-mode" 'nil :commit "64de8439e3a38e74d7f9459a6331cd94dc4d43b7" :keywords '("c" "tools"))
--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 ;;
 ;; hostごとの設定があれば読み込む
 ;;
-(setq init_host (concat "~/.emacs.d/local/init_" (system-name) ".el"))
-(if (file-exists-p init_host)
-    (load-file init_host))
+;;(setq init_host (concat "~/.emacs.d/local/init_" (system-name) ".el"))
+;;(if (file-exists-px init_host)
+;;    (load-file init_host))
 
 ;;
 ;; パッケージ管理(package.el)の設定
@@ -13,10 +13,15 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+;; ~/.emacs.d/site-lisp 以下全部読み込み
+(let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path)))
+
 ;;
 ;; init-loader
 ;;
 (require 'init-loader)
 (setq init-loader-show-log-after-init 'error-only)
 (init-loader-load "~/.emacs.d/inits")
-
